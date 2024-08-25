213fbc57ddcbcf48ddbe8cc746a45de6728da623
arushimunjal <127368251+arushimunjal@users.noreply.github.com>
2024-08-25 01:09:05 -0700

    Resolve merge conflicts

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router } from "react-router-dom";
 import AnimatedRoutes from "./components/AnimatedRoutes";
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import Navbar from './components/headers/Navbar';
+import Footer from './components/headers/Footer';
 import useLocalStorage from 'use-local-storage';
 import './App.css';
 
